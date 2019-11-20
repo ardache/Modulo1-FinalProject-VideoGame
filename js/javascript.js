9944dc770c8729fe14a41b4a650d01638af84c61
Ardash <56517452+ardache@users.noreply.github.com>
2019-11-19 22:01:17 -0600

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,55 @@
+// CONFIGURACION
+// 1.0Variables
+// 1.1Stage
+
+// 2.0Constantes
+
+
+// CLASES Y METODOS
+// 1.0Jugadores
+// 1.1Hombre
+// 1.2Mujer
+// 1.3Niño
+// 1.4Anciano
+
+// 2.0Objetos
+// 2.1Botella
+// 2.2Musica
+// 2.3Monedas
+// 2.4Anillo
+
+// 2.5Amante
+// 2.6Sarten
+// 2.7Bebe
+
+// 2.8Colegiatura
+// 2.9Enfermedad
+// 2.10Rodilla
+
+// 2.11Medicina
+// 2.12Muerte
+
+
+// INSTANCIAS
+// crearJugador
+
+
+// FUNCIONES COMPLEMENTARIAS
+
+// GenerarObjetos
+// DibujarObjetos
+// RevisarColision
+
+
+// FUNCIONES PRINCIPALES
+
+// GameOver
+// Main
+
+
+// EVENTOS 
+
+// KeyListener
+
+
+
