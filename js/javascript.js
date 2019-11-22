b16a90d9d2e46a9a6a59f6fd0fea49fd1116b8df
Ardash <56517452+ardache@users.noreply.github.com>
2019-11-21 22:01:31 -0600

    avance Jueves
    ok

--- js/javascript.js
+++ js/javascript.js
@@ -1,37 +1,76 @@
 // CONFIGURACION
 // 1.0 Variables
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+let interval;
+let frames = 0;
+
+canvas.width = 600;
+canvas.height = 1000;
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = 'black';
+ctx.fillRect(5, 5, 60, 60);
+
 // 1.1 Stage
 
 // 2.0 Constantes
 
 
 // CLASES Y METODOS
-// 1.0 Jugadores
+// 1.0 JUGADORES
+
 // 1.1 Hombre
 // 1.2 Mujer
 // 1.3 Niño
 // 1.4 Anciano
 
-// Level 1
-// 2.0 Objetos
-// 2.1 NE Botella
-// 2.2 NE Musica
-// 2.3 Monedas
-// 2.4 DM Anillo
 
-//Level 2
-// 2.5 HD Accidente
-// 2.6 HD Sarten
-// 2.7 DM Bebe
+// 2.0 OBJETOS (X,Y,W,H)
+// CONSTRUCTOR (IMAGEN/TIPO, SPEED, BANK, COVERAGE, LEVEL, GAMEOVER)
 
-//Level 3
-// 2.8 NE Colegiatura
-// 2.9 HD Enfermedad
-// 2.10 DM Rodilla
+// BANK +
+// 2.3 BANK+ (LEVEL 1,2,3) ::: Monedas :::
+class ObjectBankUp {
+    constructor(x,y,w,h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.image = './img/coin.png'
+        this.speed = 5;
+        this.bank = 1.5;
+        this.coverage = 0;
+        this.level = 0;
+        this.gameover = 0;
+    }
+    draw() {
+        this.x += this.speed;
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+}
 
-//Level 4
-// 2.11 Medicina
-// 2.12 DM Muerte
+// BANK -
+// 2.1 BANK- (LEVEL 1) ::: Botella :::
+// 2.2 BANK- (LEVEL 1) ::: Musica :::
+// 2.8 BANK- (LEVEL 2) ::: Colegiatura :::
+// 2.11 BANK- (LEVEL 2,3,4) ::: Medicina :::
+
+// COVERAGE -
+// 2.5 BANK- COVERAGE- (LEVEL 2,3,4) ::: Accidente :::
+// 2.6 BANK- COVERAGE- (LEVEL 2,3) ::: Sarten :::
+// 2.9 BANK- COVERAGE- (LEVEL 2,3,4) ::: Enfermedad :::
+
+// LEVEL +
+// 2.7 BANK- LEVEL+ (LEVEL 1) ::: Bebe :::
+// 2.4 BANK- LEVEL+ (LEVEL 2) ::: Anillo :::
+// 2.10 BANK- COVERAGE- LEVEL+  (LEVEL 3) ::: Rodilla :::
+
+// GAME OVER
+// 2.12 GAME OVER  (LEVEL 4) ::: Muerte :::
+ 
+
 
 
 // INSTANCIAS
